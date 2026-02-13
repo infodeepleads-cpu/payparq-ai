fde23de029ca3a6551fee02543be00c4154cc4a1
Karlo <kzamic@gmail.com>
2026-02-13 22:02:33 +0100

    chore: stop committing build/web, let CI build it

--- apps/mobile-scanner/build/web/main.dart.js_9.part.js
+++
@@ -1,6 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[69]
-var z=a.updateTypes([]);(function constants(){B.eZ=new A.cV(null,4,null,null)})()};
-(a=>{a["PadEcRhGGAKsGJeBW/T7+bJuLjE="]=a.current})($__dart_deferred_initializers__);
