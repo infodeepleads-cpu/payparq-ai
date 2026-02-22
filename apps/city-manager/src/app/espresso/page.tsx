7d2539ce03b5587f6ade08aadbbd8368878bcdc0
Karlo <kzamic@gmail.com>
2026-02-22 18:58:02 +0100

    Fix espresso mobile scrolling and resources text cutoff

--- apps/city-manager/src/app/espresso/page.tsx
+++ apps/city-manager/src/app/espresso/page.tsx
@@ -5,7 +5,7 @@ import EspressoDashboard from "../../components/EspressoDashboard";
 export default function Page() {
   return (
     <div className="h-screen bg-white">
-      <div className="flex h-[calc(100vh-20px)] flex-col items-center overflow-y-auto w-full">
+      <div className="flex h-[calc(100vh-20px)] flex-col items-center overflow-y-auto w-full overflow-x-hidden">
         {/* Espresso Widget Strip */}
         <EspressoDashboard />
       </div>
