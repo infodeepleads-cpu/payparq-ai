191e81852a6056c5518c47c828fa293df526bc5a
Karlo <kzamic@gmail.com>
2026-02-23 01:02:51 +0100

    chore: update capacitor server url to production

--- apps/city-manager/capacitor.config.ts
+++ apps/city-manager/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.payparq.citymanager',
   appName: 'City Manager',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://city-manager-xi.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
