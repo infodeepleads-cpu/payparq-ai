7dc6fd797e49cd3c7f8d462d68e89bd8b898564a
Karlo <kzamic@gmail.com>
2026-06-24 09:54:17 +0200

    Revert "feat: add toggle to show/hide 'Kako radi' section per listing"
    This reverts commit 84e9a3fbc3163d95f6b51e065798aba01f8979e3.

--- apps/user-web-app/src/components/SearchPage.tsx
+++ apps/user-web-app/src/components/SearchPage.tsx
@@ -2603,7 +2603,6 @@ export function SearchPage() {
                 </div>
 
                 {/* Kako radi Dropdown */}
-                {selectedListing.verification_metadata?.show_how_it_works_enabled !== false && (
                 <div className="pt-6 border-t border-gray-200 mt-6">
                   <button
                     onClick={() => setShowHowToRedeem(!showHowToRedeem)}
@@ -2622,7 +2621,6 @@ export function SearchPage() {
                     </div>
                   )}
                 </div>
-                )}
 
                 {/* Getting There Dropdown */}
                 <div className="pt-6 border-t border-gray-200 mt-6">
@@ -3522,7 +3520,6 @@ export function SearchPage() {
               </div>
 
               {/* Kako radi */}
-              {selectedListing.verification_metadata?.show_how_it_works_enabled !== false && (
               <div className="pt-3 border-t border-gray-200 mb-4 px-4">
                 <button
                   onClick={() => setShowHowToRedeem(!showHowToRedeem)}
@@ -3539,7 +3536,6 @@ export function SearchPage() {
                   </div>
                 )}
               </div>
-              )}
 
               {/* Getting There */}
               <div className="pt-3 border-t border-gray-200 mb-4 px-4">
